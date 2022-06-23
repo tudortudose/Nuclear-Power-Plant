3a899f4d3919610e665a2654bc583b27a00ef826
pricoptudor <41987455+pricoptudor@users.noreply.github.com>
2022-06-23 14:31:47 +0300

    added-mail-btn

--- public/js/about.js
+++ public/js/about.js
@@ -30,6 +30,22 @@ function sendMail(to, title, message) {
     xhr.send();
 }
 
+var btn1 = document.getElementById("reactor_btn1");
+var btn2 = document.getElementById("reactor_btn2");
+var btn3 = document.getElementById("reactor_btn3");
+
+btn1.onclick = function(){
+    sendMail("u.alexandra.maria@gmail.com",document.getElementById("about_card_title_input1").value, document.getElementById("about_card_email_input1").value+" : "+document.getElementById("about_card_message_input1").value);
+}
+
+btn2.onclick = function(){
+    sendMail("pricoptudor2001@gmail.com",document.getElementById("about_card_title_input2").value, document.getElementById("about_card_email_input2").value+" : "+document.getElementById("about_card_message_input2").value);
+}
+
+btn3.onclick = function(){
+    sendMail("tudorel_31@yahoo.com",document.getElementById("about_card_title_input3").value, document.getElementById("about_card_email_input3").value+" : "+document.getElementById("about_card_message_input3").value);
+}
+
 Array.from(about_card_toggle).forEach(function(element) {
     element.onclick = function() {
         element.parentElement.classList.toggle('active');
